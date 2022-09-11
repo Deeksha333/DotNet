83c32fa558af18577d24db7c3bfa4a0851046961
Deeksha <31833200+Deeksha333@users.noreply.github.com>
2022-09-11 15:08:30 +0530

    .Net Core learning

---
+++ Dependency Loading/Tutorial/AppWithPlugin/Program.cs
@@ -0,0 +1,66 @@
+﻿using PluginBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AppWithPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1 && args[0] == "/d")
+                {
+                    Console.WriteLine("Waiting for any key...");
+                    Console.ReadLine();
+                }
+
+                string[] pluginPaths = new string[]
+                {
+                    // Paths to plugins to load.
+                };
+
+                IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath =>
+                {
+                    Assembly pluginAssembly = LoadPlugin(pluginPath);
+                    return CreateCommands(pluginAssembly);
+                }).ToList();
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Commands: ");
+                    foreach (ICommand command in commands)
+                    {
+                        Console.WriteLine($"{command.Name}\t - {command.Description}");
+                    }
+                }
+                else
+                {
+                    foreach (string commandName in args)
+                    {
+                        Console.WriteLine($"-- {commandName} --");
+
+                        ICommand command = commands.FirstOrDefault(c => c.Name == commandName);
+                        if (command == null)
+                        {
+                            Console.WriteLine("No such command is known.");
+                            return;
+                        }
+
+command.Execute();
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
